734627bba89d37e8e8f6d041a221f1090ee2fe1f
HBanty <bijnadatta@gmail.com>
2025-06-02 03:33:26 +0530

    update utils code

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,6 +1,6 @@
-// export const BACKEND_URL = "http://localhost:4001/api/v1";
+export const BACKEND_URL = "http://localhost:4001/api/v1";
 
 
-export const BACKEND_URL = "https://coursesent.onrender.com";
+// export const BACKEND_URL = "https://coursesent.onrender.com";
 
 // pase render link herehttps://coursesent.onrender.com/
