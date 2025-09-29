6402b8c6661afb325dff39119bcc291a22309a1e
HBanty <bijnadatta@gmail.com>
2025-09-29 16:11:48 +0530

    Update dependencies and utils before redeployment

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,6 +1,6 @@
-// export const BACKEND_URL = "http://localhost:4001/api/v1";
+export const BACKEND_URL = "http://localhost:4001/api/v1";
 
 
-export const BACKEND_URL = "https://coursesent.onrender.com/api/v1";
+// export const BACKEND_URL = "https://coursesent.onrender.com/api/v1";
 
 // pase render link herehttps://coursesent.onrender.com/
