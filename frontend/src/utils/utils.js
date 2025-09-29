c9d8812f2d8ac3c5eada6c7b54a2f9d23ec9a207
HBanty <bijnadatta@gmail.com>
2025-09-29 17:08:51 +0530

    push url before deploye

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -2,6 +2,6 @@
 
 
 // export const BACKEND_URL = "https://coursesent.onrender.com/api/v1";
-export const BACKEND_URL = "https://coursesentweb.onrender.com";
+export const BACKEND_URL = "https://coursesentweb.onrender.com/api/v1";
 
 // pase render link herehttps://coursesent.onrender.com/
